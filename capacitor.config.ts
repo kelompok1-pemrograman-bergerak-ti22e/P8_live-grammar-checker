9b4eef17c5dd4efff9667b000c1507cd88270db6
safarizkyfaddillah <safarizkyfaddillah09@gmail.com>
2026-02-21 11:22:40 +0700

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'live-grammar-checker',
+  webDir: 'www'
+};
+
+export default config;
